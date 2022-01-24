9f6031423d9ea9b46a011cdec5a7fb8fdd07ba8e
yologger <yologger1013@gmail.com>
2022-01-24 14:17:30 +0900

    Add koin

--- app/src/main/java/com/yologger/smemo/App.kt
+++ app/src/main/java/com/yologger/smemo/App.kt
@@ -1,6 +1,24 @@
 package com.yologger.smemo
 
 import android.app.Application
+import com.yologger.smemo.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf(
+                appModule
+            ))
+        }
+    }
 }

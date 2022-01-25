4f2894790737ec1c8d3190663b243733c49a96a5
yologger <yologger1013@gmail.com>
2022-01-25 15:21:58 +0900

    Add Logger

--- app/src/main/java/com/yologger/smemo/App.kt
+++ app/src/main/java/com/yologger/smemo/App.kt
@@ -5,20 +5,45 @@ import com.yologger.smemo.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 
-class App: Application() {
+import com.orhanobut.logger.FormatStrategy
+
+
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         setupKoin()
+        setupLogger()
     }
 
     private fun setupKoin() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules(listOf(
-                appModule
-            ))
+            modules(
+                listOf(
+                    appModule
+                )
+            )
         }
+    }
+
+    private fun setupLogger() {
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            // .showThreadInfo(false) // (Optional) Whether to show thread info or not. Default true
+            // .methodCount(0) // (Optional) How many method line to show. Default 2
+            // .methodOffset(7) // (Optional) Hides internal method calls up to offset. Default 5
+            // .logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
+            // .tag("PRETTY_LOGGER") // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .build()
+
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
     }
 }
--- app/src/main/java/com/yologger/smemo/presentation/screen/create/CreateActivity.kt
+++ app/src/main/java/com/yologger/smemo/presentation/screen/create/CreateActivity.kt
@@ -2,11 +2,13 @@ package com.yologger.smemo.presentation.screen.create
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.orhanobut.logger.Logger
 import com.yologger.smemo.R
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        Logger.w("onCreate() From CreateActivity")
     }
 }
